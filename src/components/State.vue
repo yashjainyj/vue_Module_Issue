f3059b430e9e0024647518eada9763d5af260819
Yash jain <32011627+yashjainyj@users.noreply.github.com>
2024-08-13 18:03:57 +0530

    Add files via upload

---
+++ src/components/State.vue
@@ -0,0 +1,37 @@
+<template>
+  <select id="state-select" class="rounded-md bg-green-200 text-white" name="state">
+    <option
+      v-for="(state, index) in states"
+      :selected="isSelected(state)"
+      :key="index"
+    >
+      {{ state }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  props: ["todo", "index"],
+  data() {
+    return {
+      name: "",
+      state: "",
+      states: ["Pending", "In Progress", "Done"],
+    };
+  },
+  created() {
+    if(!this.todo) return true;
+
+    this.state = this.todo.state;
+    this.name = this.todo.name;
+  },
+  methods: {
+    isSelected(equivalent){
+      return equivalent === this.state;
+    }
+  }
+};
+</script>
+
+<style scoped></style>
